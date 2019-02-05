03eab57c49381685831d26586d50542f71df75ef
github username <github email address>
2019-02-05 16:44:00 +0000

    with adding a toy now working, need to add on server fully and finish likes functionality

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const addBtn = document.querySelector('#new-toy-btn')
 const toyForm = document.querySelector('.container')
 const toyCollection = document.querySelector('#toy-collection')
 // let addToy = false
+const inputName = document.querySelector('#inputName')
+const inputImage = document.querySelector('#inputImage')
 
 const state = {
   toys: [],
@@ -23,6 +25,13 @@ function renderSingleToy(toy) {
 
 function renderMultipleToys(toys) {
   toys.forEach(toy => renderSingleToy(toy))
+
+  toyForm.addEventListener('submit', (event) => {
+    event.preventDefault()
+    createAToy()
+    addAToy()
+
+  })
 }
 // YOUR CODE HERE
 
@@ -38,9 +47,30 @@ addBtn.addEventListener('click', () => {
 })
 
 
-// OR HERE!
+// add a new Toy
+function createAToy() {
+  const toyEl = document.createElement('div')
+  toyEl.className = 'card'
+  const name = inputName.value
+  const image = inputImage.value
 
+  toyEl.innerHTML = `
+        <h2>${name}</h2>
+        <img src=${image} class="toy-avatar" />
+        <p>0 likes </p>
+        <button class="like-btn">Like <3</button>
+  `;
+  toyCollection.appendChild(toyEl)
+}
 //
+
+// like a Toy
+
+function likeAToy() {
+  document.addEventListener('click', event => {
+    
+  })
+}
 
 function initialize() {
   getToys()
@@ -50,11 +80,33 @@ function initialize() {
     })
 }
 
-
 // server stuff
 function getToys() {
   return fetch('http://localhost:3000/toys')
             .then(resp => resp.json())
+}
+
+function addAToy(name, image) {
+  state.toys.push({name: name, image: image, likes: 0})
+  return fetch('http://localhost:3000/toys', {
+    method: 'POST',
+    headers: { 'Content-Type' : 'application/json' },
+    body: JSON.stringify({ name: name, image: image, likes: 0 })
+  }).then(resp => resp.json)
+}
+
+function updateToy(toy) {
+  return fetch(`http://localhost:3000/toys/${toy.id}`, {
+    method: 'PATCH',
+    headers: { 'Content-Type' : 'application/json' },
+    body: JSON.stringify(toy)
+  }).then(resp => resp.json())
+}
+
+function deleteAToy(toy) {
+  return fetch(`http://localhost:3000/toys/${toy.id}`, {
+    method: 'DELETE'
+  }).then(resp => resp.json())
 }
 
 
