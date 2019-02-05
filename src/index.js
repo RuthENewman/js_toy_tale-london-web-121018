87cb109980627656db6a7e04fe1af6e1c3cd4d92
github username <github email address>
2019-02-05 15:27:23 +0000

    steps 1 to 3 completed, need to add the POST and PATCH requests to complete everything

--- src/index.js
+++ src/index.js
@@ -1,13 +1,35 @@
 const addBtn = document.querySelector('#new-toy-btn')
 const toyForm = document.querySelector('.container')
-let addToy = false
+const toyCollection = document.querySelector('#toy-collection')
+// let addToy = false
 
+const state = {
+  toys: [],
+  addToy: false
+}
+
+function renderSingleToy(toy) {
+  const div = document.createElement('div')
+  div.className = 'card'
+  div.innerHTML = `
+        <h2>${toy.name}</h2>
+        <img src=${toy.image} class="toy-avatar" />
+        <p>${toy.likes} likes </p>
+        <button class="like-btn">Like <3</button>
+  `;
+  toyCollection.appendChild(div)
+}
+
+
+function renderMultipleToys(toys) {
+  toys.forEach(toy => renderSingleToy(toy))
+}
 // YOUR CODE HERE
 
 addBtn.addEventListener('click', () => {
   // hide & seek with the form
-  addToy = !addToy
-  if (addToy) {
+  state.addToy = !state.addToy
+  if (state.addToy) {
     toyForm.style.display = 'block'
     // submit listener here
   } else {
@@ -17,3 +39,23 @@ addBtn.addEventListener('click', () => {
 
 
 // OR HERE!
+
+//
+
+function initialize() {
+  getToys()
+    .then(toys => {
+      state.toys = toys
+      renderMultipleToys(state.toys)
+    })
+}
+
+
+// server stuff
+function getToys() {
+  return fetch('http://localhost:3000/toys')
+            .then(resp => resp.json())
+}
+
+
+initialize()
