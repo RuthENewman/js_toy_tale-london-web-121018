14068e31bccde389e15f21aabcbe9df9584d210c
github username <github email address>
2019-02-06 10:41:33 +0000

    fully working all steps completed

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const inputImage = document.querySelector('#inputImage')
 
 const state = {
   toys: [],
-  addToy: false
+  selectedToy: null
 }
 
 function renderSingleToy(toy) {
@@ -16,8 +16,9 @@ function renderSingleToy(toy) {
   div.innerHTML = `
         <h2>${toy.name}</h2>
         <img src=${toy.image} class="toy-avatar" />
-        <p>${toy.likes} likes </p>
-        <button class="like-btn">Like <3</button>
+        <p class="num-of-likes">${toy.likes} likes </p>
+        <button class="like-btn" data-id="${toy.id}">Like <3</button>
+        <!-- <button class="delete-btn" data-id="${toy.id}">Delete toy</button> -->
   `;
   toyCollection.appendChild(div)
 }
@@ -29,7 +30,6 @@ function renderMultipleToys(toys) {
   toyForm.addEventListener('submit', (event) => {
     event.preventDefault()
     createAToy()
-    addAToy()
 
   })
 }
@@ -57,21 +57,54 @@ function createAToy() {
   toyEl.innerHTML = `
         <h2>${name}</h2>
         <img src=${image} class="toy-avatar" />
-        <p>0 likes </p>
+        <p class="num-of-likes">0 likes </p>
         <button class="like-btn">Like <3</button>
+        <!-- <button class="delete-btn">Delete</button> -->
   `;
-  toyCollection.appendChild(toyEl)
+  toyCollection.appendChild(toyEl);
+  addAToy(name, image)
 }
 //
 
-// like a Toy
+// like a Toy event listener
 
-function likeAToy() {
+// function likeAToy() {
   document.addEventListener('click', event => {
-    
-  })
+    if (event.target.className === 'like-btn') {
+      const id = parseInt(event.target.dataset.id)
+      const foundToy = state.toys.find(toy => toy.id === id)
+      foundToy.likes += 1
+      // foundToy = state.selectedToy
+      // state.selectedToy.likes += 1
+      // debugger
+      updateToy(foundToy)
+      updateToyCard(foundToy)
+      // updateToy(state.selectedToy);
+  }
+})
+
+// update toy card for likes
+
+function updateToyCard(toy) {
+  event.target.parentElement.innerHTML = `
+        <h2>${toy.name}</h2>
+        <img src=${toy.image} class="toy-avatar" />
+        <p class="num-of-likes">${toy.likes} likes </p>
+        <button class="like-btn" data-id="${toy.id}">Like <3</button>
+        `;
 }
 
+// delete a toy event listener
+
+
+  // document.addEventListener('click', event => {
+  //   const id = parseInt(event.target.dataset.id)
+  //   const foundToy = state.toys.find(toy => toy.id === id)
+  //   deleteAToy(foundToy)
+  //
+  // })
+
+//
 function initialize() {
   getToys()
     .then(toys => {
